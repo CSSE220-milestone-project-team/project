fb84ea942e6fbfd58f62a33e70b3963fa9c736ee
linf <linf@rose-hulman.edu>
2022-05-03 18:47:39 -0400

    enemy class set!

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,2 +1,19 @@
 public class Enemy {
+
+    public boolean catchPlayer(int PlayerX,int PlayerY, int EnemyX, int EnemyY){
+
+        return false;
+    }
+
+
+
+    public void moveLeft(int locX,int distance){
+        locX=locX-distance;
+
+    }
+    public void moveRight(int locX, int distance){
+        locX=locX+distance;
+
+    }
+
 }
