32e18cb6e29d43a7d9686db3ac10b8e56d72292a
Willem De Veirman <deveirws@rose-hulman.edu>
2022-05-05 13:33:04 -0400

    Name added.

--- src/Object.java
+++ src/Object.java
@@ -1,5 +1,13 @@
 public class Object {
 
+    private Object object;
+
+    private int posX;
+
+    private int posY;
+
+    private int speed;
+
     public Object(){
         //construct object
     }
--- src/Viewer.java
+++ src/Viewer.java
@@ -9,7 +9,9 @@ public class Viewer extends JFrame {
     }
 
     public Viewer(){
+        final String frameTitle = "Bomb Jack";
         JFrame frame = new JFrame();
+        frame.setTitle(frameTitle);
         frame.setSize((new Dimension(600, 400)));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
